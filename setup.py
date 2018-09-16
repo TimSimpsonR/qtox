0e06691b6aa5faabf9aa8f33c8386b994d8a1b6e
Tim Simpson <timsimpson4@gmail.com>
2018-09-16 03:54:07 -0500

    Bump version: 0.0.4 → 0.0.5

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except BaseException:
 
 setuptools.setup(
     name="qtox",
-    version="0.0.4",
+    version="0.0.5",
     author="Tim Simpson",
     description="Lets you re-run Tox commands faster and in parallel.",
     long_description=long_description,
