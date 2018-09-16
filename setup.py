a7b31eb0ea9d5982e35a08445942c9f28fe99332
Tim Simpson <timsimpson4@gmail.com>
2018-09-16 03:54:03 -0500

    more setup.py stuff for pypi

--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
+import pathlib
 import setuptools
+
+
+simple_description = "Lets you re-run Tox commands faster and in parallel."
+try:
+    with open(pathlib.Path(__file__).parent / "README.rst") as file:
+        long_description = file.read()
+except BaseException:
+    long_description = simple_description
 
 
 setuptools.setup(
@@ -6,9 +15,10 @@ setuptools.setup(
     version="0.0.4",
     author="Tim Simpson",
     description="Lets you re-run Tox commands faster and in parallel.",
+    long_description=long_description,
     packages=setuptools.find_packages(exclude=["tests"]),
     include_package_data=True,
     install_requires=["typing"],
-    license='MIT',
+    license="MIT",
     entry_points={"console_scripts": ["qtox = qtox.main:main"]},
 )
